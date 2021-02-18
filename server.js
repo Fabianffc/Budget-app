df879138234201e7dc3898351b8689f55f24d435
Fabian <fabianffc@gmail.com>
2021-02-18 16:46:58 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+//requiring dependencies 
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+//setting the port for the app in localhost to 3002
+const PORT = 3002;
+
+const app = express();
+
+app.use(logger("dev"));
+//compression make that the app would work without internet
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
